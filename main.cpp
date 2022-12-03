22de09bd25c114ce83fd54f49d50473de8b57abb
shaluo.zzy <shaluo.zzy@alibaba-inc.com>
2022-12-03 18:42:57 +0800

    feat: 优化计数排序

--- main.cpp
+++ main.cpp
@@ -1,88 +1,51 @@
 #include <iostream>
 
-int maxbit(int* a, int len) {
-    // 先找最大的数
+void countingSort(int* a, int n) {
     int max = a[0];
-    for (int i = 1; i < len; i++) {
+    int min = a[0];
+    for (int i = 0; i < n; i++) {
         if (a[i] > max) {
             max = a[i];
         }
-    }
-
-    // 然后对最大的数求位数d
-    int d = 1;
-    for (int i = 0;; i++) {
-        max /= 10;
-        if (max == 0) {
-            break;
+        if (a[i] < min) {
+            min = a[i];
         }
-        d++;
     }
-    return d;
+    int len = max - min;
+    int* count = new int[len]; // 创建计数数组
+    for (int i = 0; i < n; i++) {
+        count[a[i]-min]++;
+    }
+    int index = 0;
+    for (int i = 0; i < len; i++) { // 遍历计数数组，回填数字
+        while(count[i] > 0) {
+            a[index] = i+min;
+            index++;
+            count[i]--;
+        }
+    }
 }
-
-void radixSort(int* a, int n) {
-    int d = maxbit(a, n);
-    int count[10]; // 创建计数数组
-    int* tmp = new int[n]; // 创建tmp数组
-    int radix = 1; // 位数初始为1
-    for (int i = 1; i <= d; i++) { // 从个位数开始遍历每个位数， 每个位数进行计数排序， 然后回填到原数组
-        // 个位数
-        for (int j = 0; j < 10; j++) { // 初始化计数数组
-            count[j] = 0;
-        }
-        for (int j = 0; j < n; j++) { // 遍历每个数， 分离数位
-            int sw = a[j] / radix % 10;
-            count[sw]++;
-        }
-
-        // 精髓来了， 因为++有累计， 所以要用空间打平， 也就是说需要构建一个tmp数组，将打平后的有序结果保存， ++类似压入， 回填的时候需要反着来， 先取n-1
-        // 1. count数组按照值进行打平
-        for (int j = 1; j < 10; j++) {
-            count[j] = count[j] + count[j-1]; // 把值就转换成了打平的初始index
-            // count[0] = 1, count[1] = 2, count[3] = 2, count[4] = 5
-        }
-        // 从后往前回填
-        for (int j = n - 1; j >= 0; j--) {
-            int sw = a[j] / radix % 10;
-            int index = count[sw] - 1; // 回填索引开始值为count[sw]-1
-            tmp[index] = a[j];
-            // 最后一步把count--，取相同数位的第二个数
-            count[sw]--;
-        }
-
-        // tmp数组即为针对数位i排序好的临时数组，回填到原数组里面，保留稳定性
-        for (int j = 0; j < n; j++) {
-            a[j] = tmp[j];
-        }
-
-        // radix基数x10
-        radix = radix * 10;
-    }
-
-    // 释放内存
-    delete[] tmp;
-}
-
 
 
 using namespace std;
-
-int main() {
+int main()
+{
 
     /**
-     * 基数排序
-     * 将所有待比较数值(注意,必须是正整数)统一为同样的数位长度,数位较短的数前面补零.
-     * 然后, 从最低位开始, 依次进行一次稳定排序(常用计数排序算法, 因为每个位可能的取值范围是固定的从0到9).
-     * 这样从最低位排序一直到最高位排序完成以后, 数列就变成一个有序序列.
+     * 计数排序
+     *  1. 找出最大值最小值
+     *  2. 按照范围初始化计数数组 count[n]
+     *  3. 遍历原数组中的元素， 值作为索引， 出现一次数值+1
+     *  4. 回填数组
 
-        适用范围： 正整数， 数据可以稀疏也可以不均匀，适合很大的数比较
-        时间复杂度： O(n)
+     适用范围： 自然数 0和正整数，数字不多且分布均匀密集
+     时间复杂度： O(n)
+
      */
 
-    int a[10] = {50, 1, 58, 22, 84, 199, 20, 89, 9, 100};
-    int len = sizeof(a) / sizeof(a[0]);
-    radixSort(a, len);
+    int a[10] = {50, 0, 58, 22, 84, 199, 20, 89, 9, 100};
+    int len = sizeof(a)/sizeof(a[0]);
+    countingSort(a, len);
 
     for (int i = 0; i < len; i++) {
         cout << a[i] << " ";
--- 排序算法/计数排序.cpp
+++ 排序算法/计数排序.cpp
@@ -1,40 +1,29 @@
 #include <iostream>
-#include <climits>
 
-int* countingSort(const int *input_arr, int len) {
-    // 计算最大值最小值
-    int max = 0;
-    int min = INT_MAX;
-    for(int i = 0; i < len; i++) {
-        int number = input_arr[i];
-        if (number > max) {
-            max = number;
+void countingSort(int* a, int n) {
+    int max = a[0];
+    int min = a[0];
+    for (int i = 0; i < n; i++) {
+        if (a[i] > max) {
+            max = a[i];
         }
-        if (number < min) {
-            min = number;
+        if (a[i] < min) {
+            min = a[i];
         }
     }
-    // 初始化计数数组, 默认值都为0
-    int* count_arr = new int[max-min+1];
-    // 遍历数值范围
-    for (int i = 0; i < len; i++) {
-        count_arr[input_arr[i]]++;
+    int len = max - min;
+    int* count = new int[len]; // 创建计数数组
+    for (int i = 0; i < n; i++) {
+        count[a[i]-min]++;
     }
-
-    // 升序排序
-    int ii = 0;
-    int *result_arr = new int[len];
-    for (int i = 0; i < max-min+1; i++) {
-        while(count_arr[i] > 0) {
-            result_arr[ii] = i+min;
-            ii++;
-            count_arr[i]--;
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+        while(count[i] > 0) {
+            a[index++] = i+min;
+            count[i]--;
         }
     }
-    return result_arr;
-
 }
-
 
 
 using namespace std;
@@ -43,14 +32,10 @@ int main()
 
     /**
      * 计数排序
-     * 基础版
-     *
-     * 第一步：找出原数组中元素值最大的，记为max。
-第二步：创建一个新数组count，其长度是max加1，其元素默认值都为0。
-第三步：遍历原数组中的元素，以原数组中的元素作为count数组的索引，以原数组中的元素出现次数作为count数组的元素值。
-第四步：创建结果数组result，起始索引index。
-第五步：遍历count数组，找出其中元素值大于0的元素，将其对应的索引作为元素值填充到result数组中去，每处理一次，count中的该元素值减1，直到该元素值不大于0，依次处理count中剩下的元素。
-第六步：返回结果数组result。
+     *  1. 找出最大值最小值
+     *  2. 按照范围初始化计数数组 count[n]
+     *  3. 遍历原数组中的元素， 值作为索引， 出现一次数值+1
+     *  4. 回填数组
 
      适用范围： 自然数 0和正整数，数字不多且分布均匀密集
      时间复杂度： O(n)
@@ -59,10 +44,10 @@ int main()
 
     int a[10] = {50, 0, 58, 22, 84, 199, 20, 89, 9, 100};
     int len = sizeof(a)/sizeof(a[0]);
-    int* result_arr = countingSort(a, len);
+    countingSort(a, len);
 
     for (int i = 0; i < len; i++) {
-        cout << result_arr[i] << " ";
+        cout << a[i] << " ";
     }
     cout << endl;
 
